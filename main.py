1d58a713fb305d321eeda30461e6d773239e420c
jiak1 <jackdonaldson005@gmail.com>
2020-05-01 10:38:24 +0000

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ app = Flask(__name__, static_url_path="", static_folder="static")
 
 @app.route("/") #Create a page on the first url
 def homePage():
-	return render_template("homepage.html")
+	items = [
+		"Cook Dinner",
+		"Wash Up",
+		"Do Laundry",
+		"Clean Room"
+	]
+	return render_template("homepage.html",todolist=items)
 	
 app.run(host="0.0.0.0")
