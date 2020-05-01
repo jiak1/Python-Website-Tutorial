f920a911881d330506854666ccb47e477511c5b6
jiak1 <jackdonaldson005@gmail.com>
2020-05-01 08:44:48 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
 
 @app.route("/") #Create a page on the first url
 def homePage():
