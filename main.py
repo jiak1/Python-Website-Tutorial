5629bc5d45ddf668f3865514aa8fac2fe464ffc4
jiak1 <jackdonaldson005@gmail.com>
2020-05-01 07:17:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/") #Create a page on the first url
+def homePage():
+	return render_template("homepage.html")
+	
+app.run(host="0.0.0.0")
