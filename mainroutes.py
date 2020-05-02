1edbf85c0ac9f931a78f9e1c7cde15820215f123
jiak1 <jackdonaldson005@gmail.com>
2020-05-02 03:08:30 +0000

    Initial commit

--- mainroutes.py
+++ mainroutes.py
@@ -1,10 +1,23 @@
-from flask import render_template,Blueprint
+from flask import render_template,Blueprint,redirect,url_for
 from models import TodoList,TodoItem
+from program import db
 
 routes = Blueprint('routes', __name__)
 
+@routes.route('/lists')
+def showLists():
+	lists = TodoList.query.all()
+	return render_template("showlists.html",todolists=lists)
+
+@routes.route("/addlist")
+def addList():
+	newList = TodoList()
+	newList.name = "New Todo List"
+	db.session.add(newList)
+	db.session.commit()
+	return redirect(url_for('routes.showLists'))
+
 @routes.route('/')
-@routes.route('/index')
 def index():
 	items = [
 		"Cook Dinner",
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from program import db
 class TodoList(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50),nullable=False,default="My Todo List")
-	items = db.relationship('todo_item',backref='list',lazy='dynamic')
+	items = db.relationship('TodoItem',backref='list',lazy='dynamic')
 
 class TodoItem(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
